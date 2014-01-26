c2e27f2ea436818cd70273b1f73666902f61e1a0
strixcuriosus <strixcuriosus@gmail.com>
2014-01-25 22:36:47 -0800

    complete multiple sum m35

---
+++ m35.js
@@ -0,0 +1,23 @@
+function mult35(num) {
+    function sum(array){
+        var ans = 0;
+        for (var i = 0, len = array.length; i < len; i++){
+            ans += array[i];
+        }
+
+        return ans;
+    }
+
+    var multiples =[];
+    for (var j = 1; j < num; j++){
+        if ((j % 3 == 0) || (j % 5 == 0)){
+            multiples.push(j);
+        }
+    }
+
+    return sum(multiples);
+
+}
+
+console.log(mult35(10));
+console.log(mult35(1000));
