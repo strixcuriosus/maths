2d5f6c7174c78fb8c9d6c8b661312e6fbd0ac49f
Ash Hoover <strixcuriosus@gmail.com>
2014-06-25 00:21:07 -0700

    Implements a breadth-first search method on the Tree prototype

--- bfsearch.js
+++ bfsearch.js
@@ -1,4 +1,4 @@
-var Tree = function(value) {
+var Tree = function (value) {
   this.value = value;
   this.children = [];
 }
@@ -9,3 +9,39 @@ Tree.prototype.addChild = function (value) {
   return child;
 }
 
+Tree.prototype.breadthFirstSearch = function (target) {
+  var resultNode = null;
+  var nodesToCheck = [this];
+  var currentNode; 
+  /*
+   when breadthFirstSearch is invoked as a tree method, 
+   the keyword this will refer to the tree object, i.e. the root node
+  */
+
+  var checkNode = function(node) {
+    if(node.value === target) {
+      resultNode = node;
+    }
+  }
+
+  while((!resultNode) && nodesToCheck.length > 0) {
+    currentNode = nodesToCheck.shift();
+    for (var i = 0; i < currentNode.children.length; i++) {
+      nodesToCheck.push(currentNode.children[i]);
+    }
+    checkNode(currentNode);
+  }
+
+  return resultNode; //returns the node whose value matches target
+}
+
+
+/*
+var tree = new Tree(12);
+var child1 = tree.addChild(30);
+var child2 = tree.addChild(50);
+var grandchild = child1.addChild(999);
+var greatgrandchild = grandchild.addChild(42);
+
+tree.breadthFirstSearch(42); // {value: 42, children: []} 
+*/
